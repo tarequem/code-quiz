1539f1ebdc41c824d611db6d799f111d9e480ba3
tarequem <moatartareque@gmail.com>
2022-05-28 23:53:52 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+//timer that counts down quiz
+var quizScore = 0;
+var answers = [];
+var saveScore = [];
+
+//function that starts the quiz by initiating delete function on intro content in main and added first question content
+
+//function that submits new question after deleting previous one
+    //button.addeventlistner("click", question2); Event Bubling
+
+//append child questionX to div
+
+//matches function
+
+//function that deletes questions. var delete = questions.querySelector()
+
+//function that stores choice into an array
+
+//function that sees if choices array matches correct answers array, points counted based on that 
+
+//takes value of quizScore counter and adds it to saveScore
+
+//load array from local storage
+
+//displays scores in header
+
+//"matches, plus 1" then move onto next question. 
+
+//answer list array
+
+//function where user inputs initials to store score
+
+//function that stores score
+
+//function that resets the quiz
